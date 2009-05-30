d8d4d575378b5cdd1045e1da52b9e4744e84356c
Mutwin Kraus <mutle@blogage.de>
2009-05-30 20:34:59 +0200

    Initial commit to rack-uploads.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rack-uploads'
+
+Spec::Runner.configure do |config|
+  
+end
