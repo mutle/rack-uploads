07d98cc6962f86211bf70e47849c656f08da83e9
Mutwin Kraus <mutle@blogage.de>
2009-08-07 20:41:13 +0200

    Refactored parameter parsing for nested parameters

--- lib/rack/uploads/middleware.rb
+++ lib/rack/uploads/middleware.rb
@@ -7,17 +7,23 @@ module Rack
       @nginx = options[:nginx] || [{ :tmp_path => "_tmp_path", :filename => "_file_name" }]
     end
 
+    def extract_uploads_from_arguments(req, params, uploads=[])
+      params.each do |key,param|
+        if param_file = file(req, key, param)
+          uploads << param_file
+          params[key] = param_file
+        elsif params.instance_of?(Hash)
+          uploads = extract_uploads_from_arguments(req, param, uploads)
+        end
+      end
+      uploads
+    end
+
     def call(env)
       req = Rack::Request.new(env)
       if req.post? && req.form_data?
         return not_authorized unless @session_authorized == true || (@session_authorized.respond_to?(:call) && @session_authorized.call(req) == true)
-        uploads = []
-        req.params.each do |key,param|
-          if param_file = file(req, key)
-            uploads << param_file
-            req.params[key] = param_file
-          end
-        end
+        uploads = extract_uploads_from_arguments(req, req.params)
         env['rack.uploads'] = uploads if uploads.size > 0
       end
       resp = @app.call(env)
@@ -31,8 +37,7 @@ module Rack
       req.media_type == "multipart/form-data"
     end
 
-    def file(req, key)
-      param = req.params[key]
+    def file(req, key, param)
       return UploadedFile.new(key, param) if param.instance_of?(Hash) && param[:tempfile]
       @nginx.each do |nginx|
         if key =~ %r{^(.+)#{nginx[:tmp_path]}$}
